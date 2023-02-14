8f62bf20e61b70194507d54ad9ee30e2d864ebe7
Adrian H <multyx.pratix@gmail.com>
2023-02-14 21:54:03 +0100

    Setup test environment

---
+++ Energy/src/test/java/fr/uparis/TestMain.java
@@ -0,0 +1,13 @@
+package fr.uparis;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMain {
+    @Test
+    public void testSum() {
+        //assertEquals(1, 0);
+        Main.main(new String[2]);
+    }
+}
