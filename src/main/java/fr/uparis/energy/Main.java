826f1489bf8eebda33a45d3594d5b74030a65be9
DERVISHI <sevidervishi@outlook.fr>
2023-02-23 15:48:24 +0100

    tryed to set up auto format

--- src/main/java/fr/uparis/energy/Main.java
+++ src/main/java/fr/uparis/energy/Main.java
@@ -1,6 +1,8 @@
 package fr.uparis.energy;
 
 public class Main {
+    public static void Test() {}
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
