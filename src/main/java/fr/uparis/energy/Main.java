6fc64442299c93fe69dd7d6029df1541e915f154
Adrian H <multyx.pratix@gmail.com>
2023-02-24 17:47:00 +0100

    Try new gradle.yml

--- src/main/java/fr/uparis/energy/Main.java
+++ src/main/java/fr/uparis/energy/Main.java
@@ -1,7 +1,9 @@
 package fr.uparis.energy;
 
 public class Main {
-    public static void Test() {}
+    public static void Test() {
+
+    }
 
     public static void main(String[] args) {System.out.println("Hello world!");
     }
