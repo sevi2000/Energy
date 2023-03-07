b4ddeba29c62eac27be3b61fe33700fee14e532a
DERVISHI <sevidervishi@outlook.fr>
2023-03-07 23:20:02 +0100

    applyed formater.

--- src/test/java/fr/uparis/energy/TestBoard.java
+++ src/test/java/fr/uparis/energy/TestBoard.java
@@ -1,20 +1,16 @@
 package fr.uparis.energy;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import fr.uparis.energy.model.Board;
 import fr.uparis.energy.model.Geometry;
 import fr.uparis.energy.model.InvalidSizeException;
 import fr.uparis.energy.model.Level;
 import fr.uparis.energy.utils.InvalidLevelException;
 import fr.uparis.energy.utils.LevelConverter;
-import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class TestBoard {
 
@@ -38,43 +34,40 @@ public class TestBoard {
 
     @Test
     public void testBoardConstructorException() {
-        assertThrows(InvalidSizeException.class,
-                () -> {
-                    Board board = new Board(0,0, Geometry.SQUARE);
-                }
-        );
-
+        assertThrows(InvalidSizeException.class, () -> {
+            Board board = new Board(0, 0, Geometry.SQUARE);
+        });
     }
 
     @Test
     public void testAddColumnAtLeft() throws InvalidSizeException {
-        Board b = new Board(1,1,Geometry.HEXAGON);
+        Board b = new Board(1, 1, Geometry.HEXAGON);
         int expected = b.getWidth() + 1;
         b.addColumnAtLeft();
-        assertEquals(expected,b.getWidth());
+        assertEquals(expected, b.getWidth());
     }
 
     @Test
     public void testAddRowAtBottom() throws InvalidSizeException {
-        Board b = new Board(1,1,Geometry.HEXAGON);
+        Board b = new Board(1, 1, Geometry.HEXAGON);
         int expected = b.getHeight() + 1;
         b.addRowAtBottom();
-        assertEquals(expected,b.getHeight());
+        assertEquals(expected, b.getHeight());
     }
 
     @Test
     public void testRemoveColumnAtLeft() throws InvalidSizeException {
-        Board b = new Board(1,1,Geometry.HEXAGON);
+        Board b = new Board(1, 1, Geometry.HEXAGON);
         int expected = b.getWidth() - 1;
         b.removeColumnAtLeft();
-        assertEquals(expected,b.getWidth());
+        assertEquals(expected, b.getWidth());
     }
 
     @Test
     public void testRemoveRowAtBottom() throws InvalidSizeException {
-        Board b = new Board(1,1,Geometry.HEXAGON);
+        Board b = new Board(1, 1, Geometry.HEXAGON);
         int expected = b.getHeight() - 1;
         b.removeRowAtBottom();
-        assertEquals(expected,b.getHeight());
+        assertEquals(expected, b.getHeight());
     }
 }
