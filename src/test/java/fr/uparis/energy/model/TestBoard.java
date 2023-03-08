5ed72593c298193a6da8118b81e6151a10d2c796
Adrian H <multyx.pratix@gmail.com>
2023-03-08 10:30:41 +0100

    Re-add tests

---
+++ src/test/java/fr/uparis/energy/model/TestBoard.java
@@ -0,0 +1,78 @@
+package fr.uparis.energy.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import fr.uparis.energy.utils.InvalidLevelException;
+import fr.uparis.energy.utils.LevelConverter;
+import java.io.IOException;
+import java.net.URL;
+import org.junit.jupiter.api.Test;
+
+public class TestBoard {
+
+    @Test
+    public void testBoardSolvedInASolvedLevel() throws InvalidSizeException, IOException, InvalidLevelException {
+        URL levelLocation = getClass().getClassLoader().getResource("levels/level1.nrg");
+        Level l = LevelConverter.fileToLevel(levelLocation, Level.State.PLAYING);
+        l.getBoard().propagateEnergy();
+        assertTrue(l.isSolved());
+    }
+
+    @Test
+    public void testBoardSolvedInANonSolvedLevel() throws InvalidSizeException, IOException, InvalidLevelException {
+        URL levelLocation = getClass().getClassLoader().getResource("levels/level3.nrg");
+        Level l = LevelConverter.fileToLevel(levelLocation, Level.State.PLAYING);
+        if (l.start()) {
+            l.getBoard().propagateEnergy();
+            // Files.writeString(Path.of("/tmp/energy.log"), l.toStringWithEnergy());
+            assertFalse(l.isSolved());
+        }
+    }
+
+    @Test
+    public void testShuffleWorks() {
+        Level l = LevelConverter.getLevelFromResources(4, Level.State.PLAYING);
+        Level l2 = LevelConverter.getLevelFromResources(4, Level.State.PLAYING);
+        l2.start();
+        assertNotEquals(l.toString(), l2.toString());
+    }
+
+    @Test
+    public void testBoardConstructorException() {
+        assertThrows(InvalidSizeException.class, () -> {
+            Board board = new Board(0, 0, Geometry.SQUARE);
+        });
+    }
+
+    @Test
+    public void testAddColumnAtLeft() throws InvalidSizeException {
+        Board b = new Board(1, 1, Geometry.HEXAGON);
+        int expected = b.getWidth() + 1;
+        b.addColumnAtLeft();
+        assertEquals(expected, b.getWidth());
+    }
+
+    @Test
+    public void testAddRowAtBottom() throws InvalidSizeException {
+        Board b = new Board(1, 1, Geometry.HEXAGON);
+        int expected = b.getHeight() + 1;
+        b.addRowAtBottom();
+        assertEquals(expected, b.getHeight());
+    }
+
+    @Test
+    public void testRemoveColumnAtLeft() throws InvalidSizeException {
+        Board b = new Board(2, 2, Geometry.HEXAGON);
+        int expected = b.getWidth() - 1;
+        b.removeColumnAtLeft();
+        assertEquals(expected, b.getWidth());
+    }
+
+    @Test
+    public void testRemoveRowAtBottom() throws InvalidSizeException {
+        Board b = new Board(2, 2, Geometry.HEXAGON);
+        int expected = b.getHeight() - 1;
+        b.removeRowAtBottom();
+        assertEquals(expected, b.getHeight());
+    }
+}
---
+++ src/test/java/fr/uparis/energy/utils/TestLevelConverter.java
@@ -0,0 +1,31 @@
+package fr.uparis.energy.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import fr.uparis.energy.model.InvalidSizeException;
+import fr.uparis.energy.model.Level;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.jupiter.api.Test;
+
+public class TestLevelConverter {
+    @Test
+    public void testEmptyIsSorted() {
+        assertTrue(LevelConverter.isStrictlySorted(new int[0]));
+    }
+
+    @Test
+    public void testNotSorted() {
+        assertFalse(LevelConverter.isStrictlySorted(new int[] {5, 3, 8}));
+    }
+
+    @Test
+    public void testFileToLevel() throws URISyntaxException, IOException, InvalidSizeException, InvalidLevelException {
+        URL levelLocation = getClass().getClassLoader().getResource("levels/level11.nrg");
+        Level l = LevelConverter.fileToLevel(levelLocation, Level.State.PLAYING);
+        assertEquals(Files.readString(Path.of(levelLocation.toURI())), l.toString());
+    }
+}
