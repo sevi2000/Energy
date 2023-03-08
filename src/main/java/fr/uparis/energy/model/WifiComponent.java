b70f9f133e7fb31984c052c8b2f5f5115f3affff
Adrian H <multyx.pratix@gmail.com>
2023-03-08 09:05:23 +0100

    Remove comments in WifiComponent

--- src/main/java/fr/uparis/energy/model/WifiComponent.java
+++ src/main/java/fr/uparis/energy/model/WifiComponent.java
@@ -4,17 +4,6 @@ package fr.uparis.energy.model;
  * Represents a wifi component.
  */
 public class WifiComponent extends Component {
-
-    // private boolean isPoweredByAnother;
-
-    /**
-     * Allows to turn this Wi-Fi hotspot on if at least one Wi-Fi hotspot is powered by a wire.
-     * @param state true if the Wi-Fi can be powered.
-     */
-    /*public void setPoweredByAnother(boolean state) {
-        this.isPoweredByAnother = state;
-    }*/
-
     /**
      * Textual representation.
      * @return a string to be written in level file.
