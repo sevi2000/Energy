afdffe92917bf53d2b7dffb43198e6c67a159c37
DERVISHI <sevidervishi@outlook.fr>
2023-03-16 18:58:11 +0100

    Refactored Component to be an enum.

--- src/main/java/fr/uparis/energy/model/Component.java
+++ src/main/java/fr/uparis/energy/model/Component.java
@@ -1,19 +1,43 @@
 package fr.uparis.energy.model;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
- * Class representing a component held by a Tile.
+ * Enum representing a component held by a Tile.
  */
-public abstract class Component {
+public enum Component {
+    SOURCE("S"),
+    LAMP("L"),
+    WIFI("W"),
+    EMPTY(".");
 
-    /**
-     * Representation of a component.
-     * @return a String representing this component.
-     */
-    public abstract String toString();
+    private static final Map<String, Component> map = new HashMap<>();
 
-    public static List<String> getKinds() {
-        return List.of(new String[] {"S", "L", "W", "."});
+    static {
+        for (Component c : values()) {
+            map.put(c.toString(), c);
+        }
+    }
+
+    private final String label;
+
+    Component(String label) {
+        this.label = label;
+    }
+
+    public static Component getFromLabel(String label) {
+        return map.get(label);
+    }
+
+    @Override
+    public String toString() {
+        return this.label;
+    }
+
+    public static List<Component> valuesAsList() {
+        return Arrays.stream(Component.values()).toList();
     }
 }
