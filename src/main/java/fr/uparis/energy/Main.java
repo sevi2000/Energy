24afedb5ae369d31ccf47235dda35bf1f75dc457
GitHub Action <action@github.com>
2023-03-20 11:16:19 +0000

    Formatted code

--- src/main/java/fr/uparis/energy/Main.java
+++ src/main/java/fr/uparis/energy/Main.java
@@ -3,6 +3,6 @@ package fr.uparis.energy;
 public class Main {
 
     public static void main(String[] args) {
-        //TODO : We need to start GUI for this method to be implemented.
+        // TODO : We need to start GUI for this method to be implemented.
     }
 }
