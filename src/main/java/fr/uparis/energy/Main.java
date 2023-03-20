b33d39d7e867a5e55d6b3d7ad52356b9c6d47fcc
DERVISHI <sevidervishi@outlook.fr>
2023-03-20 12:13:45 +0100

    It remains 20min of debpt which can be acceptable.

--- src/main/java/fr/uparis/energy/Main.java
+++ src/main/java/fr/uparis/energy/Main.java
@@ -1,17 +1,8 @@
 package fr.uparis.energy;
 
-import fr.uparis.energy.model.InvalidSizeException;
-import fr.uparis.energy.model.Level;
-import fr.uparis.energy.utils.InvalidLevelException;
-import fr.uparis.energy.utils.LevelConverter;
-import java.io.IOException;
-
 public class Main {
-    public static void Test() {}
 
-    public static void main(String[] args) throws InvalidSizeException, IOException, InvalidLevelException {
-        Level l = LevelConverter.fileToLevel(
-                Main.class.getClassLoader().getResource("levels/level3.nrg"), Level.State.PLAYING);
-        // LevelConverter.writeLevelToFile(l);
+    public static void main(String[] args) {
+        //TODO : We need to start GUI for this method to be implemented.
     }
 }
