378bd1f335cea318b7d7b34ebbdc0aa1d7c778c0
DERVISHI <sevidervishi@outlook.fr>
2023-04-19 12:24:29 +0200

    Removed view attribute from controller got it from mouseevent.

--- src/main/java/fr/uparis/energy/controller/BoardController.java
+++ src/main/java/fr/uparis/energy/controller/BoardController.java
@@ -11,17 +11,15 @@ import java.util.Map;
 public class BoardController extends MouseAdapter {
     
     private Board board;
-    private BoardView boardView;
     
     public BoardController(Board board, BoardView boardView) {
         this.board = board;
-        this.boardView = boardView;
     }
     @Override
     public void mouseClicked(MouseEvent e) {
         double minDistance = Double.POSITIVE_INFINITY;
         IntPair clickedPolygon = new IntPair(0,0);
-        for(Map.Entry<IntPair,IntPair> entry : boardView.getCoordinateMap().entrySet()) {
+        for(Map.Entry<IntPair,IntPair> entry :((BoardView)(e.getSource())).getCoordinateMap().entrySet()) {
             double distance = Math.sqrt(Math.pow(entry.getKey().a - e.getX(),2) +
                     Math.pow(entry.getKey().b - e.getY(),2));
             
@@ -29,7 +27,6 @@ public class BoardController extends MouseAdapter {
                 minDistance = distance;
                 clickedPolygon = entry.getValue();
             }
-                    //e.getX() e.getY() et entry
         }
         board.rotateTileClockWise(clickedPolygon);
         board.propagateEnergy();
